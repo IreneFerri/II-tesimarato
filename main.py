72dc91186c4cef861e6fcf18aef6996c2b3aa6fd
IreneFerri <61179062+IreneFerri@users.noreply.github.com>
2023-06-08 18:30:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,5 @@ from glob import glob
 if st.button('Clear Uploaded File(s)') and 'key' in st.session_state.keys():
     st.session_state.pop('key')
     st.experimental_rerun()
-    st.files_number_loaded.pop
-    
-    files_number_loaded = glob("*.dat")
-    st.write(f"Files loaded = {files_number_loaded}")
-    st.write(f"st.session_state.key")
+    st.experimental_rerun()
 
