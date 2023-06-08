4e4656a4886d3208f7b1190c93fc774d524d3571
IreneFerri <61179062+IreneFerri@users.noreply.github.com>
2023-06-08 16:38:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import os
+import numpy as np
+import uuid
+from glob import glob
+
+
+
 if st.button('Clear Uploaded File(s)') and 'key' in st.session_state.keys():
     st.session_state.pop('key')
     st.experimental_rerun()
+    
+files_number_loaded = glob("*.dat")
+st.write(f"Files loaded = {files_number_loaded}")
     
 
