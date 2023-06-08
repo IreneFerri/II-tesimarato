b3cba2766ca9c22e3d7b5320ddcc0736a80c1631
IreneFerri <61179062+IreneFerri@users.noreply.github.com>
2023-06-08 16:26:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 with st.form("my-form", clear_on_submit=True):
         file = st.file_uploader("upload file")
-        submitted = st.form_submit_button("submit")import streamlit as st
+        submitted = st.form_submit_button("submit")
