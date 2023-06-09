2096108b90c01c51e1f1d0ec912770b7f9fa445d
IreneFerri <61179062+IreneFerri@users.noreply.github.com>
2023-06-09 05:55:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ if st.button('Compute mean/2'):
             var = current_file.read()          
             var = float(var)
             mean = mean + var
-            st.write('user ', my_file, ' ha enviat el nombre ', var)
+            st.write("L'USUARI    " ', my_file, '    ha enviat el nombre ', var)
     half_mean = mean/(2*total_inputs)
-    st.write('result is: ', half_mean)
+    st.write('LA MEITAT DE LA MITJANA ES: ', half_mean)
 
 if st.button('check state'):
     for thing in st.session_state:
