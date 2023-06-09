8dc1306b5dd45c1c57e02896f9a14f48bf30392f
IreneFerri <61179062+IreneFerri@users.noreply.github.com>
2023-06-09 05:54:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if st.button('Compute mean/2'):
             var = current_file.read()          
             var = float(var)
             mean = mean + var
-            st.write('user ', current_file, ' ha enviat el nombre ', var)
+            st.write('user ', my_file, ' ha enviat el nombre ', var)
     half_mean = mean/(2*total_inputs)
     st.write('result is: ', half_mean)
 
